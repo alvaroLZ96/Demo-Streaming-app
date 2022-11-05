da23bfc7a4da7b5d28460db545e96708143531b1
alvaroLZ96 <alvarost54@gmail.com>
2022-11-06 00:42:20 +0100

    modal y css

--- app/src/components/Popup.jsx
+++ app/src/components/Popup.jsx
@@ -1,4 +1,4 @@
-/* import React from "react";
+/*import React from "react";
 import "./modal.css";
 
 const Popup = ({ open, onClose, item }) => {
--- app/src/pages/SERIES/Series.jsx
+++ app/src/pages/SERIES/Series.jsx
@@ -4,12 +4,16 @@ import { getMovie } from "../../services/API";
 import { Link } from "react-router-dom";
 import "./styles.css";
 import { filterandsort } from "../../utils/filterandsort";
-/* import Popup from "../../components/Popup"; */
 
 const Series = () => {
   const [seriesList, setSeriesList] = useState([]);
   const [openModal, setOpenModal] = useState(false);
-  const [modalData, setModalData] = useState(null);
+  const [popupContent, setPopupContent] = useState([]);
+
+  const changeContent = (serie) => {
+    setPopupContent([serie]);
+    setOpenModal(!openModal);
+  };
 
   useEffect(() => {
     (async () => {
@@ -22,40 +26,58 @@ const Series = () => {
     })();
   }, []);
   return (
-    <div>
-      <h1> SERIES</h1>
-      <ul>
-        {seriesList /* .slice(0, 20) */
-          .map((serie) => (
-            <li key={serie.title}>
-              <button
-                className="modalButton"
-                onClick={() => {
-                  setOpenModal(true);
-                  setModalData(serie);
-                }}
-              >
-                <h3>{serie.title}</h3>
-              </button>
-              {/*  <Link to={`/series/${serie.title}`}> */}
-              <img
-                src={serie.images["Poster Art"].url}
-                alternative={serie.title}
-              />
-              <Popup
-                item={serie}
-                open={openModal}
-                onClose={() => setOpenModal(false)}
-              >
-                <h1>{modalData.title}</h1>
-                <img src={modalData.images["Poster Art"].url} />
-              </Popup>
-              {/*     </Link> */}
-            </li>
-          ))}
-      </ul>
-      {/* <button>al pulsar aquí me hace un slice(0,40)</button> */}
-    </div>
+    <>
+      <h1 className="seriesTitle"> POPULAR SERIES</h1>
+      <div className="divList">
+        <ul className="seriesList">
+          {seriesList /* .slice(0, 20) */
+            .map((serie) => (
+              <li key={serie.title}>
+                <img
+                  src={serie.images["Poster Art"].url}
+                  alternative={serie.title}
+                />
+                <button
+                  className="modalButton"
+                  onClick={() => {
+                    changeContent(serie);
+                    //setOpenModal(true);
+                  }}
+                >
+                  <h3>{serie.title}</h3>
+                </button>
+              </li>
+            ))}
+        </ul>
+        {openModal && (
+          <div className="popupContainer" onClick={changeContent}>
+            <div className="popupbody" onClick={(e) => e.stopPropagation()}>
+              <div className="popupHeader">
+                <button onClick={changeContent}>X</button>
+              </div>
+              <div className="popupContent">
+                {popupContent.map((pop) => {
+                  return (
+                    <div className="popupCard">
+                      <h3>Name: {pop.title}</h3>
+                      <p>Description:{pop.description}</p>
+                      <p>Release Year:{pop.releaseYear}s</p>
+                      <img src={pop.images["Poster Art"].url} />
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          </div>
+        )}
+        {/*  <Link to={`/series/${serie.title}`}> */}
+        {/* <Popup item={serie} open={openModal} onClose={() => setOpenModal(false)}>
+        <h1>{serie.title}</h1>
+        <img src={serie.images["Poster Art"].url} />
+      </Popup> */}
+        {/* <button>al pulsar aquí me hace un slice(0,40)</button> */}
+      </div>
+    </>
   );
 };
 
