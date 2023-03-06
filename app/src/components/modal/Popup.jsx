ce16a708048e4997019464f4e13b47fd85b9622b
alvaroLZ96 <113988129+alvaroLZ96@users.noreply.github.com>
2023-03-06 21:03:47 +0100

    Update Popup.jsx

--- app/src/components/modal/Popup.jsx
+++ app/src/components/modal/Popup.jsx
@@ -27,33 +27,3 @@ const Popup = ({ open, onClose, items }) => {
   }
 };
 export default Popup;
-
-/* const Popup = ({ open, onClose, item }) => {
-  if (!open) return null;
-  console.log(item);
-  return (
-    <div onClick={onClose} className="overlay">
-      <div
-        onClick={(e) => {
-          e.stopPropagation();
-        }}
-        className="modalContainer"
-      >
-        <div className="modalRight">
-          <p className="closeBtn" onClick={onClose}>
-            X
-          </p>
-          <div className="content">
-            <h3>Name: {item.title}</h3>
-            <p>Description:{item.description}</p>
-            <p>Release Year:{item.releaseYear}s</p>
-            <img src={item.images["Poster Art"].url} />
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export default Popup;
- */
