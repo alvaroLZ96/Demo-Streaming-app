f07dc473fd2cace7669a4e81246271181f5e9eb4
alvaroLZ96 <alvarost54@gmail.com>
2023-03-30 22:49:54 +0200

    cors

--- server/index.js
+++ server/index.js
@@ -1,11 +1,16 @@
 const jsonServer = require("json-server");
-const cors = require("cors");
 const server = jsonServer.create();
 const router = jsonServer.router("data.json");
 const middlewares = jsonServer.defaults();
+const cors = require("cors");
 
 server.use(cors());
 server.use(middlewares);
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 server.use(router);
 server.listen(3030, () => {
   console.log("JSON Server is running");
